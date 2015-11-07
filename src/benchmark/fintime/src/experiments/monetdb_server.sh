64050167bfbde38ce39351f84c979953c9a29441
Jose Cambronero <jose.cambronero@cs.nyu.edu>
2015-11-06 23:32:52 -0500

    got embedded python to work

--- src/benchmark/fintime/src/experiments/monetdb_server.sh
+++ src/benchmark/fintime/src/experiments/monetdb_server.sh
@@ -1,16 +1,31 @@
 if [ $# -ne 1 ]; then
-	echo "Usage: -start | -stop"
+	echo "Usage: -init -start | -stop"
 	exit 1
 fi
 
-if [ $1 == "-start" ];
+# initialize for the first time
+if [ $1 == "-init" ];
 	then
+		# create databases and set embedded python permission
 		monetdbd create tables/monetdb
 		monetdbd start tables/monetdb
 		monetdb create fintime
+		monetdb set embedded_py=true fintime
 		monetdb release fintime
 		# Set up monetdb config file with monetdb user
 		echo -e "user=monetdb\npassword=monetdb" > .monetdb
+		monetdbd stop tables/monetdb	
+fi		
+
+
+# start mserver5 with embedded python
+if [ $1 == "-start" ];
+	then
+		if [ ! -d tables/monetdb ]; then
+			echo "Must run ./monetdb_server.sh -init first"
+			exit 1
+		fi
+	monetdbd start tables/monetdb
 fi
 
 if [ $1 == "-stop" ];
